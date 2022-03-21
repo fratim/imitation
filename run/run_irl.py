a977c0237456342f5e692c6a919d32287a55ad49
fratim <frtim@robots.ox.ac.uk>
2022-03-21 11:59:43 +0000

    updated hydra config things to allow choosing specific demonstrations independet of learner

---
+++ run/run_irl.py
@@ -0,0 +1,17 @@
+import hydra
+
+from utils import hydra_to_sacred
+
+from imitation.scripts import train_adversarial, eval_policy
+
+
+@hydra.main(config_path="../configs/", config_name="config.yaml")
+def main(config):
+    # run training of IRL policy
+    train_adversarial.main_console(hydra_to_sacred(config.sacred["train_adversarial"]))
+
+    # evaluate RL policy and save video
+    eval_policy.main_console(hydra_to_sacred(config.sacred["eval_irl_policy"]), flag="IRL")
+
+if __name__ == "__main__":
+    main()
---
+++ run/run_rl.py
@@ -0,0 +1,17 @@
+import hydra
+
+from utils import hydra_to_sacred
+
+from imitation.scripts import train_rl, eval_policy
+
+
+@hydra.main(config_path="../configs/", config_name="config.yaml")
+def main(config):
+    # run training of RL policy
+    train_rl.main_console(hydra_to_sacred(config.sacred["train_rl"]))
+
+    # evaluate RL policy and save video
+    eval_policy.main_console(hydra_to_sacred(config.sacred["eval_rl_policy"]), flag="RL")
+
+if __name__ == "__main__":
+    main()
