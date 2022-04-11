removed bug in truncated network that omitted the use of a normalization layer, set actions to None as unused

src/imitation/algorithms/adversarial/common.py
@@ -434,7 +434,7 @@ class AdversarialTrainer(base.DemonstrationAlgorithm[types.Transitions]):
 
             loss = F.binary_cross_entropy_with_logits(
                 disc_logits,
-                labels.float() ## ODO make sure this is correct,
+                labels.float() ## TODO make sure this is correct,
             )
 
             # do gradient step
@@ -542,7 +542,8 @@ class AdversarialTrainer(base.DemonstrationAlgorithm[types.Transitions]):
                         n_enc_updates = int(np.around(1/self.n_enc_updates_per_round))
                     for _ in range(n_enc_updates):
                         with networks.training(self._encoder_net):
-                            self.train_enc()
+                            with networks.evaluating(self.reward_train):
+                                self.train_enc()
 
             if callback:
                 callback(r)
@@ -617,21 +618,25 @@ class AdversarialTrainer(base.DemonstrationAlgorithm[types.Transitions]):
         assert n_gen == len(gen_samples["acts"])
         assert n_gen == len(gen_samples["next_obs"])
 
-        if hasattr(self._reward_net, "target_states") and not invert_states_expert:
+        if hasattr(self._reward_net, "target_states") and not invert_states_expert and tuple(range(expert_samples["obs"].shape[1])) == self._reward_net.target_states:
+            obs = np.concatenate([expert_samples["obs"], gen_samples["obs"]])
+            acts = None
+            next_obs = np.concatenate([expert_samples["next_obs"], gen_samples["next_obs"]])
+        elif hasattr(self._reward_net, "target_states") and not invert_states_expert:
             target_states = self._reward_net.target_states
             obs = np.concatenate([expert_samples["obs"][:, target_states], gen_samples["obs"][:, target_states]])
-            acts = np.concatenate([expert_samples["acts"][:, (0, )], gen_samples["acts"][:, (0, )]]) # TODO-TF fix this
+            acts = None
             next_obs = np.concatenate([expert_samples["next_obs"][:, target_states], gen_samples["next_obs"][:, target_states]])
         elif hasattr(self._reward_net, "target_states") and invert_states_expert:
             target_states = self._reward_net.target_states
             target_states_inverted = (2, 1, 0)
             obs = np.concatenate([expert_samples["obs"][:, target_states_inverted], gen_samples["obs"][:, target_states]])
-            acts = np.concatenate([expert_samples["acts"][:, (0,)], gen_samples["acts"][:, (0,)]])  # TODO-TF fix this
+            acts = None
             next_obs = np.concatenate(
                 [expert_samples["next_obs"][:, target_states_inverted], gen_samples["next_obs"][:, target_states]])
         else:
             obs = np.concatenate([expert_samples["obs"], gen_samples["obs"]])
-            acts = np.concatenate([expert_samples["acts"], gen_samples["acts"]])
+            acts = None
             next_obs = np.concatenate([expert_samples["next_obs"], gen_samples["next_obs"]])
 
 
@@ -734,14 +739,14 @@ class AdversarialTrainer(base.DemonstrationAlgorithm[types.Transitions]):
 
 
         # TODO-TF how to treat actions? yet to be solved
-        if hasattr(self._reward_net, "target_states"):
+        if hasattr(self._reward_net, "target_states") and tuple(range(gen_samples["obs"].shape[1])) != self._reward_net.target_states:
             target_states = self._reward_net.target_states
             obs = gen_samples["obs"][:, target_states]
-            acts = gen_samples["acts"][:, (0, )] # TODO-TF fix this
+            acts = None
             next_obs = gen_samples["next_obs"][:, target_states]
         else:
-            obs = gen_samples["obs"] # TODO there was a huge bug here...
-            acts = gen_samples["acts"]
+            obs = gen_samples["obs"]
+            acts = None
             next_obs = gen_samples["next_obs"]
 
 

src/imitation/rewards/reward_nets.py
@@ -75,7 +75,7 @@ class RewardNet(nn.Module, abc.ABC):
             observations, actions, next observations and dones.
         """
         state_th = th.as_tensor(state, device=self.device)
-        action_th = th.as_tensor(action, device=self.device)
+        action_th = th.as_tensor(action, device=self.device) if action is not None else None
         next_state_th = th.as_tensor(next_state, device=self.device)
         done_th = th.as_tensor(done, device=self.device)
 
@@ -91,7 +91,8 @@ class RewardNet(nn.Module, abc.ABC):
             action_th,
             self.action_space,
             self.normalize_images,
-        )
+        ) if action_th is not None else None
+
         next_state_th = preprocessing.preprocess_obs(
             next_state_th,
             self.observation_space,
@@ -101,7 +102,8 @@ class RewardNet(nn.Module, abc.ABC):
 
         n_gen = len(state_th)
         assert state_th.shape == next_state_th.shape
-        assert len(action_th) == n_gen
+        if action_th is not None:
+            assert len(action_th) == n_gen
 
         return state_th, action_th, next_state_th, done_th
 
@@ -345,8 +347,7 @@ class BasicEncoderNet(RewardNet):
             kwargs: passed straight through to `build_mlp`.
         """
         super().__init__(observation_space, action_space)
-        # combined_size = preprocessing.get_flattened_obs_dim(observation_space)
-        combined_size = 3 # truncated version
+        combined_size = preprocessing.get_flattened_obs_dim(observation_space)  # truncated version
 
         full_build_mlp_kwargs = {
             "hid_sizes": [],
@@ -407,7 +408,8 @@ class BasicRewardNetTruncated(BasicRewardNet):
             use_state=use_state,
             use_action=use_action,
             use_next_state=use_next_state,
-            use_done=use_done
+            use_done=use_done,
+            **kwargs
         )
 
     def forward(
@@ -418,8 +420,9 @@ class BasicRewardNetTruncated(BasicRewardNet):
         done: th.Tensor,
     ):
 
-        state = state[:, self.target_states]
-        next_state = next_state[:, self.target_states]
+        if not tuple(range(state.shape[1])) == self.target_states:
+            state = state[:, self.target_states]
+            next_state = next_state[:, self.target_states]
 
         return super().forward(state, action, next_state, done)
 
@@ -431,8 +434,9 @@ class BasicRewardNetTruncated(BasicRewardNet):
         done: np.ndarray,
     ) -> np.ndarray:
 
-        state = state[:, self.target_states]
-        next_state = next_state[:, self.target_states]
+        if not tuple(range(state.shape[1])) == self.target_states:
+            state = state[:, self.target_states]
+            next_state = next_state[:, self.target_states]
 
         return super().predict(state, action, next_state, done)
 